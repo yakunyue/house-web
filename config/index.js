38636b8cba2726a59f633a9ec9d35b01ae63bf24
yueyakun <yyk_job@163.com>
2020-06-09 15:42:28 +0800

    update

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://34.92.3.80/:8083',
+        target: 'http://47.94.130.235:8083',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
